7cf4cb6f548f5d16ee83a252be5dbb80c2e34b75
mayrang <pkss0626@naver.com>
2023-06-13 10:31:54 +0900

    sanity-studio 추가

---
+++ sanity-studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '9b1ym5v5',
+    dataset: 'production'
+  }
+})
---
+++ sanity-studio/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'pink-echidna',
+
+  projectId: '9b1ym5v5',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
