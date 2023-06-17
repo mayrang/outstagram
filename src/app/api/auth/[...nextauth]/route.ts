80b7c742d9f1c7ff6c4bbd9ddd1d1cfa23059e32
mayrang <pkss0626@naver.com>
2023-06-18 00:58:41 +0900

    user sanity beta

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthOptions, Session } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
-
+import { createUser } from "@/sanity/sanity";
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
@@ -21,6 +21,18 @@ export const authOptions: NextAuthOptions = {
       }
 
       return session;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      const schemaUser = {
+        _id: user.id,
+        _type: "user",
+        name: user.name || "",
+        email: user.email || "",
+        image: user.image || "",
+        username: user.email?.split("@")[0] || "",
+      };
+      await createUser(schemaUser);
+      return true;
     },
   },
 };
