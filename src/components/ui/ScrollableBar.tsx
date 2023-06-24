4e168291d1a9e45aff9cdf962c2a92f790ad393f
mayrang <pkss0626@naver.com>
2023-06-24 19:29:15 +0900

    react-multi-carousel bug 수정 (다신 안쓴다 이거)

--- src/components/ui/ScrollableBar.tsx
+++ src/components/ui/ScrollableBar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import Carousel from "react-multi-carousel";
+import Carousel, { ArrowProps } from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+
+interface CustomLeftArrowProps extends ArrowProps {
+  myOwnStuff: string;
+  onClick: () => void;
+}
 const responsive = {
   desktop: {
     breakpoint: { max: 4000, min: 576 },
@@ -16,14 +21,7 @@ const responsive = {
 
 export default function ScrollableBar({ children }: { children: React.ReactNode }) {
   return (
-    <Carousel
-      customRightArrow={<AiOutlineRight className="z-10 w-8 cursor-pointer h-8 right-[calc(4%+1px)] absolute" />}
-      customLeftArrow={<AiOutlineLeft className="z-10 w-8 cursor-pointer h-8 left-[calc(4%+1px)] absolute" />}
-      infinite
-      containerClass="w-full flex  gap-2 z-1"
-      arrows
-      responsive={responsive}
-    >
+    <Carousel infinite containerClass="w-full flex  gap-2 z-1" arrows responsive={responsive}>
       {children}
     </Carousel>
   );
